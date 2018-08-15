7aa14242841b7a60b4e6dd7a553805cc74a94e80
nickpyett <contact@nickpyett.com>
2018-08-15 11:56:01 +0100

    Add entity to calculate and display lap times, add tests

---
+++ src/Entity/DisplayTimeEntity.js
@@ -0,0 +1,77 @@
+class DisplayTimeEntity {
+    constructor(hours, minutes, seconds, milliseconds) {
+        this.hours = this.parseIntOrZero(hours);
+        this.minutes = this.parseIntOrZero(minutes);
+        this.seconds = this.parseIntOrZero(seconds);
+        this.milliseconds = this.parseIntOrZero(milliseconds);
+    }
+
+    getTimeInSeconds() {
+        // Milliseconds is passed as an integer (so the time 01:02:03.456 will have
+        // milliseconds as 456), not a decimal, so convert it to a decimal
+        const timeMillisecondsInSeconds = parseFloat(
+            '0.' + this.prependIntWithZeroes(this.milliseconds, 3),
+            10
+        ) || 0;
+
+        return (this.hours * 3600)
+            + (this.minutes * 60)
+            + this.seconds
+            + timeMillisecondsInSeconds;
+    }
+
+    getInDisplayFormat() {
+        return this.prependIntWithZeroes(this.hours, 2)
+            + ':' + this.prependIntWithZeroes(this.minutes, 2)
+            + ':' + this.prependIntWithZeroes(this.seconds, 2)
+            + '.' + this.prependIntWithZeroes(this.milliseconds, 3);
+    }
+
+    getInTimeFormat() {
+        return this.prependIntWithZeroes(this.hours, 2)
+            + ':' + this.prependIntWithZeroes(this.minutes, 2)
+            + ':' + this.prependIntWithZeroes(this.seconds, 2);
+    }
+
+    static fromDisplayFormat(displayFormat) {
+        const timeAndMilliseconds = displayFormat.split('.');
+        const hoursMinutesSeconds = timeAndMilliseconds[0].split(':');
+
+        const [hours, minutes, seconds] = hoursMinutesSeconds;
+        const milliseconds = timeAndMilliseconds[1];
+
+        return new this(hours, minutes, seconds, milliseconds);
+    }
+
+    static fromMilliseconds(timeInMilliseconds) {
+        const hours = Math.floor(timeInMilliseconds / 1000 / 60 / 60);
+
+        timeInMilliseconds -= hours * 1000 * 60 * 60;
+
+        const minutes = Math.floor(timeInMilliseconds / 1000 / 60);
+
+        timeInMilliseconds -= minutes * 1000 * 60;
+
+        const seconds = Math.floor(timeInMilliseconds / 1000);
+
+        timeInMilliseconds -= seconds * 1000;
+
+        return new this(hours, minutes, seconds, timeInMilliseconds);
+    }
+
+    prependIntWithZeroes(intToPrepend, stringLength) {
+        let zeroes = '';
+
+        for (let i = 0; i < (stringLength - 1); i++) {
+            zeroes += '0';
+        }
+
+        return zeroes.concat(intToPrepend).slice(-stringLength);
+    }
+
+    parseIntOrZero(string) {
+        return parseInt(string, 10) || 0;
+    }
+}
+
+export default DisplayTimeEntity;
---
+++ src/Tests/Entity/DisplayTimeEntity.test.js
@@ -0,0 +1,58 @@
+import DisplayTimeEntity from 'entity/DisplayTimeEntity';
+
+test('construct and get time in seconds and display', () => {
+    const dateTimeEntity = new DisplayTimeEntity(1, 2, 3, 4);
+
+    expect(dateTimeEntity.hours).toBe(1);
+    expect(dateTimeEntity.minutes).toBe(2);
+    expect(dateTimeEntity.seconds).toBe(3);
+    expect(dateTimeEntity.milliseconds).toBe(4);
+
+    expect(dateTimeEntity.getTimeInSeconds()).toBe(3723.004);
+    expect(dateTimeEntity.getInDisplayFormat()).toBe('01:02:03.004');
+});
+
+test('create using fromDisplayFormat and get time in seconds and display', () => {
+    const dateTimeEntity1 = DisplayTimeEntity.fromDisplayFormat('01:02:03.004');
+
+    expect(dateTimeEntity1.hours).toBe(1);
+    expect(dateTimeEntity1.minutes).toBe(2);
+    expect(dateTimeEntity1.seconds).toBe(3);
+    expect(dateTimeEntity1.milliseconds).toBe(4);
+
+    expect(dateTimeEntity1.getTimeInSeconds()).toBe(3723.004);
+    expect(dateTimeEntity1.getInDisplayFormat()).toBe('01:02:03.004');
+
+    const dateTimeEntity2 = DisplayTimeEntity.fromDisplayFormat('01:02:03.400');
+
+    expect(dateTimeEntity2.hours).toBe(1);
+    expect(dateTimeEntity2.minutes).toBe(2);
+    expect(dateTimeEntity2.seconds).toBe(3);
+    expect(dateTimeEntity2.milliseconds).toBe(400);
+
+    expect(dateTimeEntity2.getTimeInSeconds()).toBe(3723.4);
+    expect(dateTimeEntity2.getInDisplayFormat()).toBe('01:02:03.400');
+});
+
+test('create using fromMilliseconds and get time in seconds and display', () => {
+    const dateTimeEntity1 = DisplayTimeEntity.fromMilliseconds(3723004);
+
+    expect(dateTimeEntity1.hours).toBe(1);
+    expect(dateTimeEntity1.minutes).toBe(2);
+    expect(dateTimeEntity1.seconds).toBe(3);
+    expect(dateTimeEntity1.milliseconds).toBe(4);
+
+    expect(dateTimeEntity1.getTimeInSeconds()).toBe(3723.004);
+    expect(dateTimeEntity1.getInDisplayFormat()).toBe('01:02:03.004');
+
+    const dateTimeEntity2 = DisplayTimeEntity.fromMilliseconds(3723400);
+
+    expect(dateTimeEntity2.hours).toBe(1);
+    expect(dateTimeEntity2.minutes).toBe(2);
+    expect(dateTimeEntity2.seconds).toBe(3);
+    expect(dateTimeEntity2.milliseconds).toBe(400);
+
+    expect(dateTimeEntity2.getInDisplayFormat()).toBe('01:02:03.400');
+    expect(dateTimeEntity2.getTimeInSeconds()).toBe(3723.400);
+
+});
