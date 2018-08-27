7bb2440ed18265667de8fcc0471f0acbbb2d988c
nickpyett <contact@nickpyett.com>
2018-08-27 22:32:24 +0100

    Add entity for competitor sorting

---
+++ src/Entity/CompetitorSortEntity.js
@@ -0,0 +1,9 @@
+class CompetitorSortEntity {
+    constructor() {
+        this.orderBy = 'ordinal';
+        this.orderByLap = null;
+        this.direction = 'asc';
+    }
+}
+
+export default CompetitorSortEntity;
