5bd6d28dd562993db6041008f40574c42e3181c1
nickpyett <contact@nickpyett.com>
2018-08-30 22:25:06 +0100

    Add about component

---
+++ src/Component/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>Sailpace is an app to recored race times.</p>
+            </div>
+        );
+    }
+}
+
+export default About;
