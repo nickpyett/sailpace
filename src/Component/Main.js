d3a14f6086976343ab46336a9d6f626bcb31abaa
nickpyett <contact@nickpyett.com>
2024-06-05 10:51:17 +0100

    Add wrapper to main and add footer

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -7,12 +7,17 @@ import Race from 'Component/Race';
 class Main extends Component {
     render() {
         return (
-            <Routes>
-                <Route path="/" element={<RaceList />} />
-                <Route path="race" element={<RaceList />} />
-                <Route path="about" element={<About />} />
-                <Route path="race/:id" element={<Race />} />
-            </Routes>
+            <>
+                <div class="p-3">
+                    <Routes>
+                        <Route path="/" element={<RaceList />} />
+                        <Route path="race" element={<RaceList />} />
+                        <Route path="about" element={<About />} />
+                        <Route path="race/:id" element={<Race />} />
+                    </Routes>
+                </div>
+                <div class="m-3 py-3 border-t border-sky-600 text-sm">&copy; Nick Pyett 2024</div>
+            </>
         );
     }
 }
