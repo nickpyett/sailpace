89e62da50b2463e0a159afe50292066ef77d5787
nickpyett <contact@nickpyett.com>
2024-06-05 10:51:39 +0100

    Add styles to header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom';
 class Header extends Component {
     render() {
         return (
-            <ul>
-                <li><Link to="/race">Race List</Link></li>
-                <li><Link to="/about">About</Link></li>
-            </ul>
+            <div class="p-3 bg-sky-400 flex gap-6 leading-7">
+                <h1 class="text-lg font-bold relative pr-[20px]">Sailpace <img src="/images/sail.svg" alt="" width="20px" class="absolute top-0 right-0" /></h1> 
+
+                <nav>
+                    <ul>
+                        <li class="inline mr-2"><Link to="/race">Race List</Link></li>
+                        <li class="inline"><Link to="/about">About</Link></li>
+                    </ul>
+                </nav>
+            </div>
         );
     }
 }
