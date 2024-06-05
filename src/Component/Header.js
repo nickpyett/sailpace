4832b196b4020236b5b487a8b61b7a984ab95bb1
nickpyett <contact@nickpyett.com>
2024-06-05 15:22:56 +0100

    Style header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -5,11 +5,11 @@ class Header extends Component {
     render() {
         return (
             <div class="p-3 bg-sky-400 flex gap-6 leading-7">
-                <h1 class="text-lg font-bold relative pr-[20px]">Sailpace <img src="/images/sail.svg" alt="" width="20px" class="absolute top-0 right-0" /></h1> 
+                <div class="text-lg font-bold relative pr-[20px]">Sailpace <img src="/images/sail.svg" alt="" width="20px" class="absolute top-0 right-0" /></div> 
 
                 <nav>
                     <ul>
-                        <li class="inline mr-2"><Link to="/race">Race List</Link></li>
+                        <li class="inline mr-3"><Link to="/race">Race List</Link></li>
                         <li class="inline"><Link to="/about">About</Link></li>
                     </ul>
                 </nav>
