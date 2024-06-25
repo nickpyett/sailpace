c9f753a600c2b1619eb6414d7c151c6ec9df031a
nickpyett <contact@nickpyett.com>
2024-06-25 22:15:15 +0100

    Update class components to functional

--- src/Component/CompetitorLap.js
+++ src/Component/CompetitorLap.js
@@ -1,24 +1,18 @@
-import { Component } from 'react';
-
-class CompetitorLap extends Component {
-    onCompetitorLapChange(e) {
-        this.props.onCompetitorLapChange(this.props.competitor, this.props.lap, e.target.value);
+export default function CompetitorLap({ competitor, lap, startDateTime, isCurrentLap, onCompetitorLapChange, onCompetitorLapSet }) {
+    function handleCompetitorLapChange(e) {
+        onCompetitorLapChange(competitor, lap, e.target.value);
     }
 
-    onCompetitorLapSetClick() {
-        this.props.onCompetitorLapSet(this.props.competitor, this.props.lap);
+    function handleCompetitorLapSetClick() {
+        onCompetitorLapSet(competitor, lap);
     }
 
-    render() {
-        const disabled = this.props.startDateTime === null || this.props.lap.time || ! this.props.isCurrentLap;
+    const disabled = startDateTime === null || lap.time || ! isCurrentLap;
 
-        return (
-            <td className="p-2 w-48 text-center">
-                <input type="text" value={this.props.lap.time} pattern="\d+:\d{2}:\d{2}\.\d{3}" onChange={this.onCompetitorLapChange.bind(this)} className="w-32 font-mono border border-gray-300 px-1" />
-                <button type="button" disabled={disabled} onClick={this.onCompetitorLapSetClick.bind(this)} className="cta-small ml-2" title={disabled ? 'Only the current lap can be set automatically' : 'Set this lap'}>set</button>
-            </td>
-        );
-    }
+    return (
+        <td className="p-2 w-48 text-center">
+            <input type="text" value={lap.time} pattern="\d+:\d{2}:\d{2}\.\d{3}" onChange={handleCompetitorLapChange} className="w-32 font-mono border border-gray-300 px-1" />
+            <button type="button" disabled={disabled} onClick={handleCompetitorLapSetClick} className="cta-small ml-2" title={disabled ? 'Only the current lap can be set automatically' : 'Set this lap'}>set</button>
+        </td>
+    );
 }
-
-export default CompetitorLap;
--- src/Component/CompetitorRow.js
+++ src/Component/CompetitorRow.js
@@ -1,57 +1,52 @@
-import { Component } from 'react';
 import CompetitorLap from 'Component/CompetitorLap';
 
-class CompetitorRow extends Component {
-    onCompetitorChangeHandler(e) {
-        this.props.onCompetitorChangeHandler(this.props.competitor, e.target.name, e.target.value);
+export default function CompetitorRow({ competitor, startDateTime, onCompetitorChangeHandler, onCompetitorLapChange, onCompetitorLapSet, onRemoveCompetitorClickHandler }) {
+    function handleCompetitorChangeHandler(e) {
+        onCompetitorChangeHandler(competitor, e.target.name, e.target.value);
     }
 
-    onRemoveCompetitorClickHandler() {
-        this.props.onRemoveCompetitorClickHandler(this.props.competitor);
+    function handleRemoveCompetitorClickHandler() {
+        onRemoveCompetitorClickHandler(competitor);
     }
 
-    render() {
-        let raceCompleted = true;
-        let previousLapHasTime = true;
+    let raceCompleted = true;
+    let previousLapHasTime = true;
 
-        const competitorLaps = this.props.competitor.laps.map(lap => {
-            if (lap.time === '') {
-                raceCompleted = false;
-            }
-
-            const competitorLap = <CompetitorLap
-                key={lap.id}
-                lap={lap}
-                competitor={this.props.competitor}
-                startDateTime={this.props.startDateTime}
-                onCompetitorLapChange={this.props.onCompetitorLapChange}
-                onCompetitorLapSet={this.props.onCompetitorLapSet}
-                isCurrentLap={previousLapHasTime}
-            />;
-
-            previousLapHasTime = !! lap.time;
-
-            return competitorLap;
-        });
-
-        if (0 === this.props.competitor.laps.length) {
+    const competitorLaps = competitor.laps.map(lap => {
+        if (lap.time === '') {
             raceCompleted = false;
         }
 
-        const completed = raceCompleted ? String.fromCharCode('10004') : '-';
+        const competitorLap = <CompetitorLap
+            key={lap.id}
+            lap={lap}
+            competitor={competitor}
+            startDateTime={startDateTime}
+            onCompetitorLapChange={onCompetitorLapChange}
+            onCompetitorLapSet={onCompetitorLapSet}
+            isCurrentLap={previousLapHasTime}
+        />;
 
-        return (
-            <tr className="border-b border-gray-300">
-                <td className="p-2 w-16 text-center">{this.props.competitor.ordinal} <button type="button" onClick={this.onRemoveCompetitorClickHandler.bind(this)} className="cta-small ml-2">x</button></td>
-                <td className="p-2 w-36"><input type="text" name="name" value={this.props.competitor.name} onChange={this.onCompetitorChangeHandler.bind(this)} className="w-32" /></td>
-                <td className="p-2 w-36"><input type="text" name="number" value={this.props.competitor.number} onChange={this.onCompetitorChangeHandler.bind(this)} className="w-32" /></td>
-                <td className="p-2 w-36"><input type="text" name="class" value={this.props.competitor.class} onChange={this.onCompetitorChangeHandler.bind(this)} className="w-32" /></td>
-                {competitorLaps}
-                <td className="p-2 w-36 text-center font-mono">{this.props.competitor.timeTotal}</td>
-                <td className="p-2 w-36 text-center font-serif">{completed}</td>
-            </tr>
-        );
+        previousLapHasTime = !! lap.time;
+
+        return competitorLap;
+    });
+
+    if (competitor.laps.length === 0) {
+        raceCompleted = false;
     }
-}
 
-export default CompetitorRow;
+    const completed = raceCompleted ? String.fromCharCode('10004') : '-';
+
+    return (
+        <tr className="border-b border-gray-300">
+            <td className="p-2 w-16 text-center">{competitor.ordinal} <button type="button" onClick={handleRemoveCompetitorClickHandler} className="cta-small ml-2">x</button></td>
+            <td className="p-2 w-36"><input type="text" name="name" value={competitor.name} onChange={handleCompetitorChangeHandler} className="w-32" /></td>
+            <td className="p-2 w-36"><input type="text" name="number" value={competitor.number} onChange={handleCompetitorChangeHandler} className="w-32" /></td>
+            <td className="p-2 w-36"><input type="text" name="class" value={competitor.class} onChange={handleCompetitorChangeHandler} className="w-32" /></td>
+            {competitorLaps}
+            <td className="p-2 w-36 text-center font-mono">{competitor.timeTotal}</td>
+            <td className="p-2 w-36 text-center font-serif">{completed}</td>
+        </tr>
+    );
+}
