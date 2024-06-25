82b79e10f3bd999662ae3a9d3e04d828862d2395
nickpyett <contact@nickpyett.com>
2024-06-25 22:24:57 +0100

    Fix method names

--- src/Component/CompetitorRow.js
+++ src/Component/CompetitorRow.js
@@ -1,11 +1,11 @@
 import CompetitorLap from 'Component/CompetitorLap';
 
 export default function CompetitorRow({ competitor, startDateTime, onCompetitorChangeHandler, onCompetitorLapChange, onCompetitorLapSet, onRemoveCompetitorClickHandler }) {
-    function handleCompetitorChangeHandler(e) {
+    function handleCompetitorChange(e) {
         onCompetitorChangeHandler(competitor, e.target.name, e.target.value);
     }
 
-    function handleRemoveCompetitorClickHandler() {
+    function handleRemoveCompetitorClick() {
         onRemoveCompetitorClickHandler(competitor);
     }
 
@@ -40,10 +40,10 @@ export default function CompetitorRow({ competitor, startDateTime, onCompetitorC
 
     return (
         <tr className="border-b border-gray-300">
-            <td className="p-2 w-16 text-center">{competitor.ordinal} <button type="button" onClick={handleRemoveCompetitorClickHandler} className="cta-small ml-2">x</button></td>
-            <td className="p-2 w-36"><input type="text" name="name" value={competitor.name} onChange={handleCompetitorChangeHandler} className="w-32" /></td>
-            <td className="p-2 w-36"><input type="text" name="number" value={competitor.number} onChange={handleCompetitorChangeHandler} className="w-32" /></td>
-            <td className="p-2 w-36"><input type="text" name="class" value={competitor.class} onChange={handleCompetitorChangeHandler} className="w-32" /></td>
+            <td className="p-2 w-16 text-center">{competitor.ordinal} <button type="button" onClick={handleRemoveCompetitorClick} className="cta-small ml-2">x</button></td>
+            <td className="p-2 w-36"><input type="text" name="name" value={competitor.name} onChange={handleCompetitorChange} className="w-32" /></td>
+            <td className="p-2 w-36"><input type="text" name="number" value={competitor.number} onChange={handleCompetitorChange} className="w-32" /></td>
+            <td className="p-2 w-36"><input type="text" name="class" value={competitor.class} onChange={handleCompetitorChange} className="w-32" /></td>
             {competitorLaps}
             <td className="p-2 w-36 text-center font-mono">{competitor.timeTotal}</td>
             <td className="p-2 w-36 text-center font-serif">{completed}</td>
