5585ee9f9054a08a2f84ba9264cc9c006778aa79
nickpyett <contact@nickpyett.com>
2024-06-30 13:33:50 +0100

    Add background gradient to header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -3,7 +3,7 @@ import sail from 'sail.png';
 
 export default function Header() {
     return (
-        <div className="p-4 bg-sky-400 flex gap-6 leading-7">
+        <div className="p-4 bg-gradient-to-b from-sky-300 to-30% to-sky-400 flex gap-6 leading-7">
             <div className="text-lg font-bold relative pr-[20px]">Sailpace <img src={sail} alt="" width="20px" className="absolute top-0 right-0" /></div>
 
             <nav>
